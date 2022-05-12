8f0f2aeb6bd3284fa63f7b85ddd40e408522734c
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-05-12 15:33:20 +0200

    qw

--- js/main.js
+++ js/main.js
@@ -4,12 +4,3 @@ document.querySelector('#date2').innerText = 'ŚR.  11'
 document.querySelector('#date3').innerText = 'WT.  10'
 document.querySelector('#date4').innerText = 'PON.  9'
 document.querySelector('#nawyk1-span').innerText = 'Programowanie'
-
-
-function Kalendarz(dzien, data){
-    this.dzien = dzien
-    this.data = data
-}
-
-let data1 = new Kalendarz('Czw. ', 12)
-console.log(data1)
